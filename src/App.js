bfa4d2a8f263eda9d5bfff52eb4cb5ab64f7c303
Bonnie <bonnienjuguna106@gmail.comgit remote add origin https://github.com/bonnie106/test.gitgit config --global user.email bonnienjuguna106@gmail.com>
2022-02-17 19:02:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+import './App.css';
+// importing components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+	// state
+	const [inputText, setInputText] = useState('');
+	const [todos, setTodos] = useState([]);
+	const [status, setStatus] = useState('all');
+	const [filteredTodos, setFilteredTodos] = useState([]);
+	// useEffect
+	useEffect(() => {
+		filterHandler();
+	}, [todos, status]);
+	// function
+	const filterHandler = () => {
+		switch (status) {
+			case 'completed':
+				setFilteredTodos(todos.filter((todo) => todo.completed === true));
+				break;
+			case 'uncompleted':
+				setFilteredTodos(todos.filter((todo) => todo.completed === false));
+				break;
+			default:
+				setFilteredTodos(todos);
+				break;
+		}
+	};
+	return (
+		<div className="App">
+			<header>
+				<h1> Todo App</h1>
+			</header>
+			<Form
+				todos={todos}
+				setTodos={setTodos}
+				inputText={inputText}
+				setInputText={setInputText}
+				setStatus={setStatus}
+			/>
+			<TodoList
+				todos={todos}
+				setTodos={setTodos}
+				filteredTodos={filteredTodos}
+			/>
+		</div>
+	);
+}
+
+export default App;
